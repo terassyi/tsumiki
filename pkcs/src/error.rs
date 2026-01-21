38e15d3c82a1ea272e6f9830ce3dc7c66271a3bc
terashima <iscale821@gmail.com>
2026-01-21 22:41:42 +0900

    Add pkcs keys rustls integration
    Signed-off-by: terashima <iscale821@gmail.com>

--- pkcs/src/error.rs
+++ pkcs/src/error.rs
@@ -8,11 +8,17 @@ pub enum Error {
     #[error("PKCS#1 error: {0}")]
     Pkcs1(#[from] crate::pkcs1::Error),
 
+    #[error("PKCS#8 error: {0}")]
+    Pkcs8(#[from] crate::pkcs8::Error),
+
     #[error("SEC1 error: {0}")]
     Sec1(#[from] crate::sec1::Error),
 
     #[error(transparent)]
     PKIXTypes(#[from] pkix_types::Error),
+
+    #[error("DER error: {0}")]
+    Der(#[from] der::error::Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
