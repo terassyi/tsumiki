356bd830b31ae76b4cd43a653dcbd0825a43c050
terashima <tomoya-terashima@cybozu.co.jp>
2026-06-16 18:15:53 +0900

    feat(crl): add crl error module
    Signed-off-by: terashima <tomoya-terashima@cybozu.co.jp>

--- crl/src/lib.rs
+++ crl/src/lib.rs
@@ -1,3 +1,5 @@
 pub use tsumiki_x509::extensions::{DistributionPoint, DistributionPointName, ReasonFlags};
 
 pub type DistributionPoints = tsumiki_x509::extensions::CRLDistributionPoints;
+
+pub mod error;
